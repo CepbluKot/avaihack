54ce021d6f00f84204f84261c86c66f6d01f1500
CepbluKot <igorcat100@gmail.com>
2022-10-23 16:32:58 +0300

    upd

---
+++ front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app id="app">
+    <!-- debug
+      {{$store.state.user}}
+      <button @click="logout">logout</button>
+    -->
+    <vue-extend-layouts />
+  </v-app>
+</template>
+
+<script>
+import VueExtendLayouts from 'vue-extend-layout'
+import { logout } from "@/auth/logout";
+
+export default {
+  name: 'App',
+  components: { VueExtendLayouts },
+  methods: {
+    logout
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: var(--v-background-base) !important;
+}
+</style>
